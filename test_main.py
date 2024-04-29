1b14fe3b14dc53a71ff5ded48952a21ac2d50811
Susan Korgen <58643266+susankorgen@users.noreply.github.com>
2024-04-28 21:18:19 -0400

    output only CSV; use unittest.TestCase, setUp(), tearDown() (#6)
    * pycharm env
    
    * use unittest.TestCase, setUp(), and tearDown()
    
    * pycharm env
    
    * write only to CSV file, stop writing text for test

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 
 import os
+import unittest
 
 from _pytest.python_api import raises
 
@@ -7,89 +8,100 @@ import error
 from main import QuickDemo
 
 
-class TestMain:
+class TestMain(unittest.TestCase):
+    output_to_cleanup:str
+
+    def setUp(self) -> None:
+        self.output_to_cleanup = ""
+
+    def tearDown(self) -> None:
+        file = self.output_to_cleanup
+        if os.path.exists(file):
+            os.remove(file)
+        folder = QuickDemo.get_folder_path(file)
+        if os.path.exists(folder):
+            os.rmdir(folder)
 
     def test_demo_all_defaults_happy(self):
-        with open("resource/echo_output.txt", "r") as sample:
+        with open("resource/expected_output.csv", "r") as sample:
+
             # read input
             obj = QuickDemo("Customer")
             demo_data = obj.run_demo()
 
             # write output
-            assert os.path.exists(obj.default_output) is False
-            display_content = obj.write_demo(demo_data=demo_data)
-            assert sample.read() == display_content
+            test_target = obj.default_output
+            assert os.path.exists(test_target) is False
+            obj.write_demo(demo_data=demo_data)
+            assert os.path.exists(test_target) is True
+            with open(test_target, "r") as target:
+                assert sample.read() == target.read()
 
-            # check output, then clean up
-            created_file = os.path.exists(obj.default_output)
-            assert created_file is True
-            if created_file:
-                os.remove(obj.default_output)
-            assert os.path.exists(obj.default_output) is False
+            # clean up
+            self.output_to_cleanup = test_target
 
     def test_demo_good_input_file_name(self):  # ?
-        with open("resource/echo_output.txt", "r") as sample:
+        with open("resource/expected_output.csv", "r") as sample:
+
             # read input
             obj = QuickDemo("Customer")
             demo_data = obj.run_demo(data_source="input/DemoSample.csv")
 
             # write output
-            assert os.path.exists(obj.default_output) is False
-            display_content = obj.write_demo(demo_data=demo_data)
-            assert sample.read() == display_content
+            test_target = obj.default_output
+            assert os.path.exists(test_target) is False
+            obj.write_demo(demo_data=demo_data)
+            assert os.path.exists(test_target) is True
+            with open(test_target, "r") as target:
+                assert sample.read() == target.read()
 
-            # check output, then clean up
-            created_file = os.path.exists(obj.default_output)
-            assert created_file is True
-            if created_file:
-                os.remove(obj.default_output)
-            assert os.path.exists(obj.default_output) is False
+            # clean up
+            self.output_to_cleanup = test_target
 
     def test_demo_bad_input_file_name(self):
-        with open("resource/echo_output.txt", "r") as sample:
+        with open("resource/expected_output.csv", "r") as sample:
             obj = QuickDemo("Customer")
             with raises(FileNotFoundError) as e:
                 obj.run_demo(data_source="DemoSample.csv")
             assert "DemoSample.csv" == e.value.filename
 
     def test_good_output_file_name(self):
-        with open("resource/echo_output.txt", "r") as sample:
+        with open("resource/expected_output.csv", "r") as sample:
+
             # read input
             obj = QuickDemo("Customer")
             demo_data = obj.run_demo(data_source="input/DemoSample.csv")
 
             # write output
-            assert os.path.exists("output/DemoDeleteMe.csv") is False
-            display_content = obj.write_demo(demo_data=demo_data, display_target="output/DemoDeleteMe.csv")
-            assert sample.read() == display_content
+            test_target = "output/DemoDeleteMe.csv"
+            assert os.path.exists(test_target) is False
+            obj.write_demo(demo_data=demo_data, display_target=test_target)
+            assert os.path.exists(test_target) is True
+            with open(test_target, "r") as target:
+                assert sample.read() == target.read()
 
-            # check output, then clean up
-            created_file = os.path.exists("output/DemoDeleteMe.csv")
-            assert created_file is True
-            if created_file:
-                os.remove("output/DemoDeleteMe.csv")
-            assert os.path.exists("output/DemoDeleteMe.csv") is False
+            # clean up
+            self.output_to_cleanup = test_target
 
     def test_good_simple_output_file_name(self):
-        with open("resource/echo_output.txt", "r") as sample:
+        with open("resource/expected_output.csv", "r") as sample:
             # read input
             obj = QuickDemo("Customer")
             demo_data = obj.run_demo(data_source="input/DemoSample.csv")
 
             # write output
-            assert os.path.exists("./DemoDeleteMe.csv") is False
-            display_content = obj.write_demo(demo_data=demo_data, display_target="DemoDeleteMe.csv")
-            assert sample.read() == display_content
+            test_target = "DemoDeleteMe.csv"
+            assert os.path.exists(test_target) is False
+            obj.write_demo(demo_data=demo_data, display_target=test_target)
+            assert os.path.exists(test_target) is True
+            with open(test_target, "r") as target:
+                assert sample.read() == target.read()
 
-            # check output, then clean up
-            created_file = os.path.exists("./DemoDeleteMe.csv")
-            assert created_file is True
-            if created_file:
-                os.remove("./DemoDeleteMe.csv")
-            assert os.path.exists("./DemoDeleteMe.csv") is False
+            # clean up
+            self.output_to_cleanup = test_target
 
     def test_bad_output_file_name_case_0(self):
-        with open("resource/echo_output.txt", "r") as sample:
+        with open("resource/expected_output.csv", "r") as sample:
             obj = QuickDemo("Customer")
             demo_data = obj.run_demo(data_source="input/DemoSample.csv")
             with raises(error.InvalidOutputFile) as e:
@@ -97,7 +109,7 @@ class TestMain:
             assert "An invalid CSV output filename or folder was provided" == e.value.message
 
     def test_bad_output_file_name_case_minus_1(self):
-        with open("resource/echo_output.txt", "r") as sample:
+        with open("resource/expected_output.csv", "r") as sample:
             obj = QuickDemo("Customer")
             demo_data = obj.run_demo(data_source="input/DemoSample.csv")
             with raises(error.InvalidOutputFile) as e:
@@ -110,16 +122,14 @@ class TestMain:
         demo_data = obj.run_demo()
 
         # write first time
-        assert os.path.exists(obj.default_output) is False
+        test_target = obj.default_output
+        assert os.path.exists(test_target) is False
         obj.write_demo(demo_data=demo_data)
 
         # overwrite second time
-        assert os.path.exists(obj.default_output) is True
+        assert os.path.exists(test_target) is True
         obj.write_demo(demo_data=demo_data)
+        assert os.path.exists(test_target) is True
 
-        # now clean up
-        created_file = os.path.exists(obj.default_output)
-        assert created_file is True
-        if created_file:
-            os.remove(obj.default_output)
-        assert os.path.exists(obj.default_output) is False
+        # clean up
+        self.output_to_cleanup = test_target
